1561c1711527e3eec7ab2754948fadb7944cda34
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2020-07-29 16:41:30 -0700

    added HomeAbout HTML structure, imported fonts partial

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import HomeAbout from "./components/HomeAbout";
+import "./styles/main.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <HomeAbout />
     </div>
   );
 }
---
+++ client/src/components/HomeAbout.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+const HomeAbout = () => {
+  return (
+    // HOME HTML STARTS HERE
+    <div className="home-about">
+      <div className="home-about__pic1-container">
+        <img
+          className="home-about__pic1"
+          src="./assets/images/contenedores_01.png"
+          alt="containers"
+        />
+      </div>
+      <div className="home-about__home-title-container">
+        <h1 className="home-about__home-title">
+          Manufacturers' Representative
+        </h1>
+      </div>
+      <div className="home-about__circular-pic1-container">
+        <div className="home-about__circular-pic1-outer-circle">
+          <img
+            className="home-about__circular-pic1"
+            src="./assets/images/deal.png"
+            alt="handshake deal"
+          />
+          <a className="home-about__circular-pic1-arrow-container" href="#">
+            <img
+              className="home-about__circular-pic1-arrow"
+              src=""
+              alt="arrow down"
+            />
+          </a>
+        </div>
+      </div>
+      <div className="home-about__pic2-container">
+        <img
+          className="home-about__pic2"
+          src="./assets/images/trailers_01.png"
+          alt="trailers"
+        />
+      </div>
+
+      {/* ABOUT PAGE HTML STARTS HERE */}
+
+      <div className="home-about__about-container">
+        <div className="home-about__circular-pic2-container">
+          <div className="home-about__circular-pic2" />
+        </div>
+        <div className="home-about__about-text">
+          <h2 className="home-about__about-title">About Us</h2>
+          <h3 className="home-about__about-subtitle">
+            Custom Solutions and Value for the Long Term
+          </h3>
+          <div className="home-about__about-descr-wrapper">
+            <div className="home-about__about-descr-indent" />
+            <div className="home-about__about-paragraph-wrapper">
+              <p className="home-about__about-paragraph">
+                Those who know us, describe our company as: international sales
+                experience and positive relationships.
+              </p>
+              <p className="home-about__about-paragraph">
+                We have been in business since 2013, and our team has more than
+                25 years of experience in Business to Business sales in Canada,
+                the U.S. and Mexico.
+              </p>
+              <p className="home-about__about-paragraph">
+                Our specialty is turning business opportunities into business
+                relationships, with a long-term vision.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeAbout;
