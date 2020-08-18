c810172d2daa42a64e5346a226a5f38689d0c8f7
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2020-08-18 15:43:34 -0700

    finished MOBILE STYLING 100%

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,15 +3,36 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <span className="footer__company">Terranova Marketing Inc.</span>
+      <div className="footer__company-wrapper">
+        <span className="footer__company-copyright">©</span>
+        <span className="footer__company-name">Terranova Marketing Inc.</span>
+      </div>
       <img
         className="footer__logo"
         src="./assets/logo/terranova_ic.svg"
         alt="terranova logo"
       />
       <div className="footer__social-wrapper">
-        <div className="footer__social-ig" />
-        <div className="footer__social-fb" />
+        <a
+          className="footer__social-icon-link"
+          href="https://www.instagram.com/terranovasales/?hl=en"
+        >
+          <img
+            className="footer__social-icon-img"
+            src="./assets/icons/ig-icon.png"
+            alt="instagram icon"
+          />
+        </a>
+        <a
+          className="footer__social-icon-link"
+          href="https://www.facebook.com/Terranovasales/"
+        >
+          <img
+            className="footer__social-icon-img"
+            src="./assets/icons/fb-icon.png"
+            alt="facebook icon"
+          />
+        </a>
       </div>
     </footer>
   );
