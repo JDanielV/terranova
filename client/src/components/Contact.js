2ef7b915168dc943430aefe02b6c7ef25eb115c4
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2020-10-27 19:32:11 -0700

    finished SERVICES & CONTACT DESKTOP - SERVICES could use a little more love

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -10,35 +10,39 @@ const Contact = () => {
     <section className="contact" id="contact">
       <div className="contact__content-wrapper">
         <h2 className="contact__title">{t("contactTitle.1")}</h2>
-        <div className="contact__img-text-wrapper">
-          <img
-            className="contact__img"
-            src="./assets/images/contacto.png"
-            alt="contact"
-          />
-          <div className="contact__text-wrapper">
-            <p className="contact__company-name">Terranova Marketing Inc.</p>
-            <div className="contact__address">
-              <p className="contact__office">{t("contactOffice.1")}</p>
-              <p className="contact__office-address1">
-                922-510 W Hastings Street
-              </p>
-              <p className="contact__office-address2">Vancouver, BC V6B 1L8</p>
-              <p className="contact__office-address3">Canada</p>
-            </div>
-            <div className="contact__address">
-              <p className="contact__office">{t("contactMailing.1")}</p>
-              <p className="contact__office-address1">
-                P.O. Box 93590 Nelson Park
-              </p>
-              <p className="contact__office-address2">Vancouver, BC V6E 4L7</p>
-              <p className="contact__office-address3">Canada</p>
+        <div className="contact__info-form-map-wrapper">
+          <div className="contact__img-text-wrapper">
+            <img
+              className="contact__img"
+              src="./assets/images/contacto.png"
+              alt="contact"
+            />
+            <div className="contact__text-wrapper">
+              <p className="contact__company-name">Terranova Marketing Inc.</p>
+              <div className="contact__address">
+                <p className="contact__office">{t("contactOffice.1")}</p>
+                <p className="contact__office-address1">
+                  922-510 W Hastings Street
+                </p>
+                <p className="contact__office-address2">Vancouver, BC V6B 1L8</p>
+                <p className="contact__office-address3">Canada</p>
+              </div>
+              <div className="contact__address">
+                <p className="contact__office">{t("contactMailing.1")}</p>
+                <p className="contact__office-address1">
+                  P.O. Box 93590 Nelson Park
+                </p>
+                <p className="contact__office-address2">Vancouver, BC V6E 4L7</p>
+                <p className="contact__office-address3">Canada</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="contact__map-form-wrapper">
-          <ContactMap />
-          <ContactForm />
+          <div className="contact__map-form-wrapper">
+            <div className="contact__map-wrapper">
+              <ContactMap />
+            </div>
+            <ContactForm />
+          </div>
         </div>
       </div>
     </section>
--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -7,76 +7,78 @@ const Services = () => {
   return (
     <section className="services" id="services">
       <div className="services__content-wrapper">
-      <div className="services__text-wrapper1">
-        <h2 className="services__title">{t("servicesTitle.1")}</h2>
-        <h3 className="services__subtitle">{t("servicesSubtitle.1")}</h3>
-      </div>
-      <img
-        className="services__hero-img"
-        src="./assets/images/terranova_bandera.png"
-        alt="terranova flag"
-      />
-      {/* SERVICES CLUSTER 1 */}
-      <div className="services__cluster1-content">
-        <div className="services__cluster1-imgs-container">
-          <div className="services__cluster1-wrapper">
-            <div className="services__cluster1-img1-container">
-              <img
-                className="services__cluster1-img1"
-                src="./assets/images/consultoria_01.png"
-                alt="strategy planning"
-              />
+        <div className="services__text-wrapper1">
+          <h2 className="services__title">{t("servicesTitle.1")}</h2>
+          <h3 className="services__subtitle">{t("servicesSubtitle.1")}</h3>
+        </div>
+        <img
+          className="services__hero-img"
+          src="./assets/images/terranova_bandera.png"
+          alt="terranova flag"
+        />
+        {/* SERVICES CLUSTER 1 */}
+        <div className="services__clusters-wrapper">
+          <div className="services__cluster1-content">
+            <div className="services__cluster1-imgs-container">
+              <div className="services__cluster1-wrapper">
+                <div className="services__cluster1-img1-container">
+                  <img
+                    className="services__cluster1-img1"
+                    src="./assets/images/consultoria_01.png"
+                    alt="strategy planning"
+                  />
+                </div>
+                <div className="services__cluster1-handshake-bg">
+                  <img
+                    className="services__cluster1-handshake"
+                    src="./assets/images/consultoria_04.png"
+                    alt="handshake"
+                  />
+                </div>
+                <div className="services__cluster1-img2-container">
+                  <img
+                    className="services__cluster1-img2"
+                    src="./assets/images/consultoria_02.png"
+                    alt="containers at port"
+                  />
+                </div>
+              </div>
             </div>
-            <div className="services__cluster1-handshake-bg">
-              <img
-                className="services__cluster1-handshake"
-                src="./assets/images/consultoria_04.png"
-                alt="handshake"
-              />
-            </div>
-            <div className="services__cluster1-img2-container">
-              <img
-                className="services__cluster1-img2"
-                src="./assets/images/consultoria_02.png"
-                alt="containers at port"
-              />
+            <div className="services__cluster1-text-wrapper">
+              <h3 className="services__cluster1-subtitle">
+                {t("servicesService1Subtitle.1")}
+              </h3>
             </div>
           </div>
-        </div>
-        <div className="services__cluster1-text-wrapper">
-          <h3 className="services__cluster1-subtitle">
-            {t("servicesService1Subtitle.1")}
-          </h3>
-        </div>
-      </div>
 
-      {/* SERVICES CLUSTER 2 */}
-      <div className="services__cluster2-content">
-        <div className="services__cluster2-imgs-container">
-          <div className="services__cluster2-wrapper">
-            <div className="services__cluster2-img1-container">
-              <img
-                className="services__cluster2-img1"
-                src="./assets/images/logistica_01.png"
-                alt="logistics"
-              />
+          {/* SERVICES CLUSTER 2 */}
+          <div className="services__cluster2-content">
+            <div className="services__cluster2-imgs-container">
+              <div className="services__cluster2-wrapper">
+                <div className="services__cluster2-img1-container">
+                  <img
+                    className="services__cluster2-img1"
+                    src="./assets/images/logistica_01.png"
+                    alt="logistics"
+                  />
+                </div>
+                <div className="services__cluster2-trailer-bg"></div>
+                <div className="services__cluster2-img2-container">
+                  <img
+                    className="services__cluster2-img2"
+                    src="./assets/images/logistica_02.png"
+                    alt="warehouse"
+                  />
+                </div>
+              </div>
             </div>
-            <div className="services__cluster2-trailer-bg"></div>
-            <div className="services__cluster2-img2-container">
-              <img
-                className="services__cluster2-img2"
-                src="./assets/images/logistica_02.png"
-                alt="warehouse"
-              />
+            <div className="services__cluster1-text-wrapper">
+              <h3 className="services__cluster1-subtitle">
+                {t("servicesService2Subtitle.1")}
+              </h3>
             </div>
           </div>
         </div>
-        <div className="services__cluster1-text-wrapper">
-          <h3 className="services__cluster1-subtitle">
-            {t("servicesService2Subtitle.1")}
-          </h3>
-        </div>
-      </div>
       </div>
     </section>
   );
