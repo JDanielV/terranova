c7dc48a39bc02404783763c818c698e8fc62669c
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2022-05-08 14:41:06 -0700

    Progress on new Carousel component

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -2,12 +2,38 @@ import React, { useState } from "react";
 import { CarouselDataObj } from "../resources/CarouselData";
 
 interface CarouselProps {
-  carouselImages: CarouselDataObj[];
+  carouselSlides: CarouselDataObj[];
 }
 
-const Carousel = ({ carouselImages }: CarouselProps) => {
+const Carousel = ({ carouselSlides }: CarouselProps) => {
+  const [current, setCurrent] = useState(0);
+
+  const nextSlide = () => {
+    setCurrent(current === carouselSlides.length ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? carouselSlides.length - 1 : current - 1);
+  };
+
   return (
     <section className="products__carousel">
+      <button className="products__slider-button-left" onClick={prevSlide}>
+        <div className="products__slider-button-left-icon" />
+      </button>
+      <button className="products__slider-button-right" onClick={nextSlide}>
+        <div className="products__slider-button-right-icon" />
+      </button>
+      {carouselSlides.map((slide: CarouselDataObj, index: number) => {
+        return (
+          <div
+            key={index}
+            className={index === current ? "slide active" : "slide"}
+          >
+            {index === current && <img src={slide.img} alt={slide.imgAlt} />}
+          </div>
+        );
+      })}
       <div className="test"></div>
     </section>
   );
