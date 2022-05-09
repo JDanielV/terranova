fe2261375052b2658ca55f6c11dc3094113afde4
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2022-05-08 18:21:23 -0700

    Updated prop name

--- client/src/components/SliderBg.js
+++ client/src/components/SliderBg.js
@@ -3,7 +3,7 @@ import React from "react";
 const SliderBg = (props) => {
   let styleClasses = ["mobile-nav-bg"];
 
-  if (!props.isCarouselOpen) styleClasses.push("bg-no-show");
+  if (!props.carouselOpen) styleClasses.push("bg-no-show");
   else styleClasses.push("bg-show");
 
   return <div className={styleClasses.join(" ")}></div>;
