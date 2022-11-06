a22881b04dcc9745088c75679dfdc27a815e28d2
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2022-11-06 01:13:55 -0700

    added new objects with new categories images

--- client/src/resources/CarouselData.ts
+++ client/src/resources/CarouselData.ts
@@ -89,3 +89,55 @@ export const veggieCansImages: CarouselDataObj[] = [
     imgAlt: "hominy corn",
   },
 ];
+
+export const furnitureImages: CarouselDataObj[] = [
+  {
+    img: "./assets/images/product-imgs/furniture1.jpg",
+    imgAlt: "chickpeas",
+  },
+  {
+    img: "./assets/images/product-imgs/furniture2.jpg",
+    imgAlt: "green tomato",
+  },
+  {
+    img: "./assets/images/product-imgs/furniture3.jpg",
+    imgAlt: "chili peppers",
+  },
+];
+
+export const automationImages: CarouselDataObj[] = [
+  {
+    img: "./assets/images/product-imgs/automation1.jpg",
+    imgAlt: "chickpeas",
+  },
+  {
+    img: "./assets/images/product-imgs/automation2.jpg",
+    imgAlt: "green tomato",
+  },
+  {
+    img: "./assets/images/product-imgs/automation3.jpg",
+    imgAlt: "chili peppers",
+  },
+];
+
+export const renewableImages: CarouselDataObj[] = [
+  {
+    img: "./assets/images/product-imgs/renewable1.jpg",
+    imgAlt: "chickpeas",
+  },
+  {
+    img: "./assets/images/product-imgs/renewable2.jpg",
+    imgAlt: "green tomato",
+  },
+];
+
+export const heavyEquipmentImages: CarouselDataObj[] = [
+  {
+    img: "./assets/images/product-imgs/heavy-equipment1.jpg",
+    imgAlt: "chickpeas",
+  },
+  {
+    img: "./assets/images/product-imgs/heavy-equipment2.jpg",
+    imgAlt: "green tomato",
+  },
+];
