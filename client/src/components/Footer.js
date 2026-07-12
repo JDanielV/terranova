57b555c15bc37b02abe45171a4f9acbedbaeaa62
Daniel Villaverde <j.danielv@outlook.com>
2026-07-12 01:08:13 -0700

    removed socials icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -12,28 +12,6 @@ const Footer = () => {
         src="./assets/logo/terranova_ic.svg"
         alt="terranova logo"
       />
-      <div className="footer__social-wrapper">
-        <a
-          className="footer__social-icon-link"
-          href="https://www.instagram.com/terranovasales/?hl=en"
-        >
-          <img
-            className="footer__social-icon-img"
-            src="./assets/icons/ig-icon.png"
-            alt="instagram icon"
-          />
-        </a>
-        <a
-          className="footer__social-icon-link"
-          href="https://www.facebook.com/Terranovasales/"
-        >
-          <img
-            className="footer__social-icon-img"
-            src="./assets/icons/fb-icon.png"
-            alt="facebook icon"
-          />
-        </a>
-      </div>
     </footer>
   );
 };
